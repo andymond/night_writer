32e0653af6417784ae3e7a2916baf0e3a6c0dc09
Andrew Dymond <andy.dymond@gmail.com>
2017-12-09 12:48:42 -0700

    Update translator & translator_test files w/ proper space definition & more characters

--- lib/translator.rb
+++ lib/translator.rb
@@ -35,28 +35,55 @@ class Translator
     "x"=>"00..00",
     "y"=>"00.000",
     "z"=>"0..000",
-    " "=>" ",
-    "shift"=>".....0"
+    "shift"=>".....0",
+    " "=>"......",
+    "!"=>"..000.",
+    "'"=>"....0.",
+    ","=>"..0...",
+    "-"=>"....00",
+    "."=>"..00.0",
+    "?"=>"..0.00",
+    "#"=>".0.000",
+    "0"=>".000..",
+    "1"=>"0.....",
+    "2"=>"0.0...",
+    "3"=>"00....",
+    "4"=>"00.0..",
+    "5"=>"0..0..",
+    "6"=>"000...",
+    "7"=>"0000..",
+    "8"=>"0.00..",
+    "9"=>".00..."
   }
+
+  def character_verification(words)
+  
+  end
 
   def split_words(words)
     words.to_s.split("")
   end
 
-  def english_to_braille_array(words)
-    english = split_words(words)
-    braille = english.map do |character|
+  def chars_to_braille_array(words)
+    chars = split_words(words)
+    braille = chars.map do |character|
       DICTIONARY[character.downcase]
     end
     braille
   end
 
-  def braille_string(words)
-    english_to_braille_array(words).join('')
-  end
-
   def threelines(words)
-    "#{braille_string(words)[0..1]}\n#{braille_string(words)[2..3]}\n#{braille_string(words)[4..5]}"
+    line_1 = chars_to_braille_array(words).map do |letter|
+      letter[0..1]
+    end
+    line_2 = chars_to_braille_array(words).map do |letter|
+      letter[2..3]
+    end
+    line_3 = chars_to_braille_array(words).map do |letter|
+      letter[4..5]
+    end
+
+    "#{line_1.join('')}\n#{line_2.join('')}\n#{line_3.join('')}"
   end
 
 end
--- test/translator_test.rb
+++ test/translator_test.rb
@@ -13,15 +13,10 @@ class TranslatorTest < MiniTest::Test
   def test_braille_array_of_strings
     translator = Translator.new
 
-    assert_equal ["0....."], translator.english_to_braille_array("a")
-    assert_equal ["0.00..", "0..0..", "0.0.0.", "0.0.0.", "0..00."], translator.english_to_braille_array("Hello")
-  end
-
-  def test_braile_string_joins_array
-    translator = Translator.new
-
-    assert_equal "0.....", translator.braille_string("a")
-    assert_equal "0..... 00.00. 00.0.. 00.000", translator.braille_string("a n d y")
+    assert_equal ["0....."], translator.chars_to_braille_array("a")
+    assert_equal ["......"], translator.chars_to_braille_array(" ")
+    assert_equal ["0.....", "0.0...", "00....", "..0.00", "0.....", "0.0...", "00...."], translator.chars_to_braille_array("123?abc")
+    assert_equal ["0.00..", "0..0..", "0.0.0.", "0.0.0.", "0..00."], translator.chars_to_braille_array("Hello")
   end
 
   def test_input_letter_output_braille_on_newlines
@@ -29,16 +24,27 @@ class TranslatorTest < MiniTest::Test
 
     assert_equal "0.\n00\n..", translator.threelines("h")
     assert_equal "0.\n.0\n00", translator.threelines("z")
-    assert_equal " \n\n", translator.threelines(" ")
+    assert_equal "..\n..\n..", translator.threelines(" ")
+    assert_equal "0..0\n000.\n....", translator.threelines("hi")
+    assert_equal "0.0.0.0.0.\n00.00.0..0\n....0.0.0.", translator.threelines("hello")
+    assert_equal "0..0..0.0.0.0.0.\n000...00.00.0..0\n..........0.0.0.", translator.threelines("hi hello")
   end
 
-  def test_input_word_output_braille_array
+  def test_character_verification_works
     skip
     translator = Translator.new
 
-    assert_equal ["0.\n00\n..", ".0\n0.\n.."], word_to_braille_array("hi")
-
+    assert_equal "0.....", translator.character_verification("a")
+    assert_equal "??????", translator.character_verification("$")
   end
 
+  def test_unexpected_input
+    skip
+    translator = Translator.new
+
+    assert_equal ["??????"], translator.chars_to_braille_array("@")
+    assert_equal ["??????", "??????", "??????", "0....."], translator.chars_to_braille_array("*&^1")
+
+  end
 
 end
