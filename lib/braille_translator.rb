daa1b97f80c584bda9b19082aab4323488498517
Andrew Dymond <andy.dymond@gmail.com>
2017-12-11 15:52:11 -0700

    Update braille_translator with more methods, can decode one line

--- lib/braille_translator.rb
+++ lib/braille_translator.rb
@@ -1,5 +1,7 @@
+require_relative 'dictionary'
+
 class BrailleDecoder
-    LINE_MAX = 160
+    LINE_MAX = 80
 
     attr_accessor :braille
 
@@ -8,20 +10,20 @@ class BrailleDecoder
   end
 
   def to_words(lines)
-    divided_lines = process_line_breaks(lines)
+    single_lines = process_line_breaks(lines)
+    divided_lines = split_line(single_lines)
     one_line_braille = three_to_one_line(divided_lines)
-    verified = verify_braille(one_line_braille)
-    characters = to_characters(verified)
+    characters = to_characters(one_line_braille)
     caps_converted = has_shifts?(characters) ? capitalize(characters) : characters
-    decode(caps_converted)
+    translate(caps_converted)
   end
 
   def process_line_breaks(lines)
     lines.split("\n\n")
   end
 
-  def split_lines(line_arrays)
-    line_arrays.split("\n")
+  def split_line(line_array)
+    line_array.split("\n")
   end
 
   def three_to_one_line(lines)
@@ -30,6 +32,50 @@ class BrailleDecoder
         line.slice!(0..1)
       end.join('')
     end
+  end
+
+  def to_characters(braille_array)
+    braille_array.map do |braille|
+      Dictionary::CHARACTERS.key(braille)
+    end
+  end
+
+  def has_shifts?(character_array)
+    character_array.any? do |character|
+      character == "shift"
+    end
+  end
+
+  def capitalize(character_array)
+    character_array.map.with_index do |character, i|
+      if character == "shift"
+        character_array[i + 1].upcase!
+        character_array.delete_at(i)
+      else
+        character
+      end
+    end
+    character_array
+  end
+
+  def character_string(character_array)
+    character_array.join('')
+  end
+
+  def inject_line_breaks(line, max = LINE_MAX)
+    if line.length > max
+        lines = line.chars.each_slice(max).to_a
+        lines.map do |character_array|
+        character_array.join
+      end.join("\n")
+    else
+      line
+    end
+  end
+
+  def translate(character_array, max = LINE_MAX)
+    line = character_string(character_array)
+    inject_line_breaks(line, max)
   end
 
 end
--- test/braille_translator_test.rb
+++ test/braille_translator_test.rb
@@ -16,11 +16,13 @@ class TranslatorTest < MiniTest::Test
     assert_equal ["0.0.\n....\n....", "0.0.\n....\n...."], translator.process_line_breaks("0.0.\n....\n....\n\n0.0.\n....\n....")
   end
 
-  def test_split_lines_splits_single_braille_lines_on_line_breakes
+  def test_split_line_splits_single_braille_lines_on_line_breakes
     translator = BrailleDecoder.new
 
-    assert_equal ["0.0.", "....", "...."], translator.split_lines("0.0.\n....\n....")
+    assert_equal ["0.0.", "....", "...."], translator.split_line("0.0.\n....\n....")
   end
+
+
 
   def test_3_to_1_line_converts_line_broken_to_no_break_strings
     skip
@@ -37,10 +39,52 @@ class TranslatorTest < MiniTest::Test
   end
 
   def test_braille_to_character_converter_works
-    skip
     translator = BrailleDecoder.new
 
     assert_equal ["a", "a"], translator.to_characters(["0.....", "0....."])
+    assert_equal ["shift", "b"], translator.to_characters([".....0", "0.0..."])
+    assert_equal [" "], translator.to_characters(["......"])
+    #assert_equal ["1", "1"], translator.to_characters(["0.....", "0....."])
+  end
+
+  def test_has_shifts_method_detects_shifts_in_array
+    translator = BrailleDecoder.new
+
+    assert_equal true, translator.has_shifts?(["shift", "b"])
+    refute_equal true, translator.has_shifts?(["a", "z"])
+  end
+
+  def test_capitalize_method_converts_shift_and_following_letter_to_caps
+    translator = BrailleDecoder.new
+
+    assert_equal ["A", "B"], translator.capitalize(["shift", "a", "shift", "b"])
+    assert_equal ["A", "B", "c", "d", "!"], translator.capitalize(["shift", "a", "shift", "b", "c", "d", "!"])
+  end
+
+  def test_character_string_returns_string_of_characters_and_spaces
+    translator = BrailleDecoder.new
+
+    assert_equal "Hi my name is", translator.character_string(["H", "i", " ", "m", "y", " ", "n", "a", "m", "e", " ", "i", "s"])
+    assert_equal "O", translator.character_string(["O"])
+  end
+
+  def test_inject_line_breaks_inserts_breaks_into_strings_based_on_max
+    translator = BrailleDecoder.new
+
+    assert_equal "Hi \nmy \nnam\ne i\ns", translator.inject_line_breaks("Hi my name is", 3)
+  end
+
+  def test_translate_takes_character_array_and_returns_lines_with_breaks
+    translator = BrailleDecoder.new
+
+    assert_equal "Hi \nmy \nnam\ne i\ns", translator.translate(["H", "i", " ", "m", "y", " ", "n", "a", "m", "e", " ", "i", "s"], 3)
+  end
+
+  def test_to_words_takes_line_of_braille_and_returns_line_of_alpha
+    skip
+    translator = BrailleDecoder.new
+
+    assert_equal "a", translator.to_words("0.\n..\n..")
   end
 
 end
